b34efcd548b12680027bd1abb6e5f6e1dbab09b3
rmorsomme <rmorsomme@gmail.com>
2021-08-19 09:42:12 -0400

    function to draw figure for experiment 3

--- R/draw_E3.R
+++ R/draw_E3.R
@@ -1,53 +1,14 @@
 
-#' Line plot of M-H acceptance rate
+#' Line plots for experiment 3
 #'
 #' @inheritParams experiment_3_acceptance_vs_rho
 #' @param results df of results from experiment 3
+#' @param var variable of the df results to visualize
+#' @param lab_y label of y-axis
+#' @param plot_id name file for the figure
 #'
 #' @return saves a figure
 #' @export
-draw_accept_rate <- function(results, path) {
-
-  results %>%
-    ggplot2::ggplot(ggplot2::aes(.data$rho, .data$accept_rate, linetype = factor(.data$S0))) +
-    ggplot2::geom_point() +
-    ggplot2::geom_line() +
-    ggplot2::ylim(0, 1) +
-    ggplot2::labs(y = "Acceptance Rate",x = expression(rho)) +
-    ggplot2::theme(text = ggplot2::element_text(size = 20))
-
-  ggplot2::ggsave("E3_accept.jpg", path = path, width = 1.61803, height = 1, scale = 5)
-
-}
-
-draw_run_time <- function(results, path) {
-
-  results %>%
-    ggplot2::ggplot(ggplot2::aes(.data$rho, .data$run_time, linetype = factor(.data$S0))) +
-    ggplot2::geom_point() +
-    ggplot2::geom_line() +
-    ggplot2::labs(y = "Run time (seconds)", x = expression(rho)) +
-    ggplot2::expand_limits(y = 0) +
-    ggplot2::theme(text = ggplot2::element_text(size = 20))
-
-  ggplot2::ggsave("E3_runtime.jpg", path = path, width = 1.61803, height = 1, scale = 5)
-
-}
-
-draw_ESS_beta <- function(results, path) {
-
-  results %>%
-    ggplot(aes(rho, ESS_beta, linetype = factor(S0))) +
-    geom_point() +
-    geom_line() +
-    labs(y = "Effective Sample Size", x = expression(rho)) +
-    expand_limits(y = 0) +
-    theme(text = element_text(size = 20))
-
-  ggsave(paste0("E3_ESS_beta.jpg"), path = path, width = 1.61803, height = 1, scale = 5)
-
-}
-
 
 draw_E3 <- function(results, path, var, lab_y, plot_id) {
 

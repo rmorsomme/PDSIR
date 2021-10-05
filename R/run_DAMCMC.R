e7a4f9bb1e0a36c0503131f6b237748807ccb593
rmorsomme <rmorsomme@gmail.com>
2021-10-04 21:26:26 -0400

    fix prior R0

--- R/run_DAMCMC.R
+++ R/run_DAMCMC.R
@@ -21,7 +21,7 @@ run_DAMCMC <- function(
   par_prior = list(
     a_beta = 0.01, b_beta = 1,
     a_gamma = 1, b_gamma = 1,
-    a_R0 = 1, b_R0 = 2,
+    a_R0 = 2, b_R0 = 2,
     a_lambda = 1, b_lambda = 1
   )
   ) {

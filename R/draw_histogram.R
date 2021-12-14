4563a157baa02305da696cdd0067cb0158d33075
rmorsomme <rmorsomme@gmail.com>
2021-12-14 14:34:31 -0500

    E5 1/gamma histogram

--- R/draw_histogram.R
+++ R/draw_histogram.R
@@ -13,8 +13,9 @@ draw_histogram <- function(df, var, plot_id, path = NULL, bins = 30) {
   g <- df %>%
     ggplot2::ggplot(ggplot2::aes(x = .data[[var]])) +
     ggplot2::geom_histogram(ggplot2::aes(y = .data$..count../sum(.data$..count..)), bins = bins) +
-    ggplot2::theme(text = ggplot2::element_text(size = 20))+
-    ggplot2::labs(y = "Proportion")
+    ggplot2::theme(text = ggplot2::element_text(size = 20)) +
+    ggplot2::labs(y = "Proportion") +
+    xlab("1/gamma") # quick and dirty
 
   ggplot2::ggsave(
     paste(plot_id, var, "hist.jpg", sep = "_"),

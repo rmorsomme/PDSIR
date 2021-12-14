8ac5ce5fe926ceaaed03a9b6461b4ae10f3dd532
rmorsomme <rmorsomme@gmail.com>
2021-12-14 13:57:57 -0500

    traceplot x-axis text

--- R/draw_traceplot.R
+++ R/draw_traceplot.R
@@ -14,7 +14,10 @@ draw_traceplot <- function(df, var, plot_id, path = NULL) {
   g <- df %>%
     ggplot2::ggplot(ggplot2::aes(x = .data$Iteration, y = .data[[var]])) +
     ggplot2::geom_line() +
-    ggplot2::theme(text = ggplot2::element_text(size = 40))
+    ggplot2::theme(
+      text = ggplot2::element_text(size = 40),
+      axis.text.x = ggplot2::element_text(size = 30)
+      )
 
   ggplot2::ggsave(
     paste(plot_id, var, "tp.jpg", sep = "_"),
